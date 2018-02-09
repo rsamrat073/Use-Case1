37f89ea74f784385de3af701fa519b0d8269efc6
rsamrat073 <rsamrat073@gmail.com>
2018-02-09 09:18:07 +0530

    Manual Commit for all queries to be used

--- EntityManager/new 2.sql
+++ EntityManager/new 2.sql
@@ -4,3 +4,40 @@ create table Product
 (id int,productName varchar,
 productDescription varchar,productPrice int,
 productModelNumber varchar, primary key(id,productName,productPrice));
+
+create table Patient
+(id int,patientName varchar,
+currentDisease varchar,previousDisease varchar,
+weight int,age int,primary key(id,patientName,currentDisease));
+
+
+create table Employee
+(id int,empName varchar,
+empCode varchar,empSalary int,technologyStack varchar,primary key(id,empName,technologyStack));
+
+
+{
+    "id": 1,
+    "productName": "Seute Bag",
+    "productDescription": "Backpack bag",
+    "productPrice": 2100,
+    "productModelNumber": "Seuete 123"
+}
+
+{
+    "id": 1,
+    "patientName": "Samrat Roy",
+    "currentDisease": "Very Sick",
+    "previousDisease": "Sick",
+    "weight": 75,
+	"age": 26
+}
+
+{
+    "id": 1,
+    "empName": "Samrat Roy",
+    "empCode": "135569",
+    "empSalary": 1500000,
+    "technologyStack": Core Java-Web
+	
+}
